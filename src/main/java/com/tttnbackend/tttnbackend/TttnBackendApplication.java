6719d77d6e1b848132cae79fae5630131e921247
ChauHuyDien <82036270+idiotman-2212@users.noreply.github.com>
2024-09-09 23:38:03 +0700

    Changes

---
+++ src/main/java/com/tttnbackend/tttnbackend/TttnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tttnbackend.tttnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TttnBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tttnbackend/tttnbackend/TttnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tttnbackend.tttnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
